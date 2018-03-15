b3f55b79e3e3b9a3b105705b989af90f613226cb
Jamila Keba <jamilaleilanikeba@gmail.com>
2018-03-15 15:26:44 -0700

    set up basic app

---
+++ server.js
@@ -0,0 +1,21 @@
+// require express and other modules
+var express = require('express'),
+    app = express(),
+    bodyParser = require('body-parser');
+
+
+/************
+* DATABASE *
+************/
+
+  // var db = require('./models');
+
+
+/**********
+ * SERVER *
+ **********/
+
+// listen on the port that Heroku prescribes (process.env.PORT) OR port 3000
+app.listen(process.env.PORT || 3000, function () {
+  console.log('Express server is up and running on http://localhost:3000/');
+});

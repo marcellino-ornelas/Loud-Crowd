e791ee4ccc63d53e91a8e0ea9038fc5f9b8058b7
Jamila Keba <jamilaleilanikeba@gmail.com>
2018-03-19 10:37:18 -0700

    fixed heroku db

--- models/index.js
+++ models/index.js
@@ -1,7 +1,7 @@
 var mongoose = require("mongoose");
 mongoose.Promise = global.Promise;
 
-mongoose.connect("mongodb://localhost/engagement");
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/engagement");
 
 module.exports.Event = require("./event");
 module.exports.User = require("./user");

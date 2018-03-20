ffee0e501353b530ebbadcc0cdeaecf2bf689ba7
Jamila Keba <jamilaleilanikeba@gmail.com>
2018-03-20 12:36:00 -0700

    working on validation

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -86,4 +86,19 @@ $(function() {
 
   }
 
+  //Contact Form - checking for errors
+    $(".submit").on("click", function(event) {
+      event.preventDefault();
+      $(".checkForError").each(function() {
+        if ($(this).val() === "") {
+          $(this).addClass("error");
+          $(".error").show();
+          $(this).siblings(".errorMessage").show();
+        } else {
+          $(this).removeClass("error");
+          $(this).siblings(".errorMessage").hide();
+        }
+      });
+    });
+
 });
